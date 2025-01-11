0c8545e0b2cd9f29d054d4a5fd132c015cbb947a
Precious Akams <preciousakams@yahoo.com>
2025-01-11 19:06:27 +0000

    .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,20 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
-  <nav className="navbar">
-    <a href="/">
-      <img className="navLogo" src="../../icons/logo.png" alt="" />
-      {' '}
-    </a>
-    <h3>Space Travelers&apos; Hub</h3>
-    <ul className="nav-links">
-      <li><NavLink to="/">Rockets</NavLink></li>
-      <li className="missionNav"><NavLink to="/missions">Missions</NavLink></li>
-      <li><NavLink to="/my-profile">My Profile</NavLink></li>
-    </ul>
-  </nav>
+  <>
+    <nav className="navbar">
+      <a href="/">
+        <img className="navLogo" src="../../icons/logo.png" alt="" />
+        {' '}
+      </a>
+      <ul className="nav-links">
+        <li><NavLink to="/">Rockets</NavLink></li>
+        <li className="missionNav"><NavLink to="/missions">Missions</NavLink></li>
+        <li><NavLink to="/my-profile">My Profile</NavLink></li>
+      </ul>
+    </nav>
+    <h3 className="title">Space Travelers&apos; Hub</h3>
+  </>
 );
 
 export default Navbar;
